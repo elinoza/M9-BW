b389c2d30e11f759573ad65f13270a640e092e8a
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-03-02 18:09:03 +0100

    profilepic upload

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,5 +1,15 @@
 const express = require("express")
 const q2m = require("query-to-mongo")
+const multer = require("multer")
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const { cloudinary } = require("../../cloudinary")
+const cloudStorage = new CloudinaryStorage({
+	cloudinary: cloudinary,
+	params: {
+		folder: "Instagram/posts",
+	},
+})
+const cloudMulter = multer({ storage: cloudStorage })
 const { authenticate, refreshToken } = require("../../auth/tools")
 const { authorize } = require("../../auth/middleware")
 
@@ -171,5 +181,46 @@ usersRouter.post("/refreshToken", async (req, res, next) => {
 		}
 	}
 })
+
+usersRouter.post(
+	"/imageUpload/:id",
+	authorize,
+	cloudMulter.single("image"),
+	async (req, res, next) => {
+		try {
+			const post = { profilePicUrl: req.file.path }
+			const author = await UserSchema.findById(req.params.id, {
+				_id: 0,
+				user: 1,
+			})
+			if (author.user.userName !== req.user.userName) {
+				const error = new Error(
+					`User does not own the Post with id ${req.params.id}`
+				)
+				error.httpStatusCode = 403
+				return next(error)
+			}
+			console.log(req.body)
+			console.log(req.file.buffer)
+			console.log("help")
+			//res.json({ msg: "image uploaded" })
+
+			const newPost = await UserSchema.findByIdAndUpdate(req.params.id, post, {
+				runValidators: true,
+				new: true,
+			})
+			if (newPost) {
+				res.status(201).send("immage updated")
+			} else {
+				const error = new Error(`Post with id ${req.params.id} not found`)
+				error.httpStatusCode = 404
+				next(error)
+			}
+		} catch (error) {
+			console.log("error", error)
+			next(error)
+		}
+	}
+)
 
 module.exports = usersRouter
