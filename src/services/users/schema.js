7d3638277579000b3f023aea799bec3552f55173
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-03-03 16:09:18 +0100

    Update schema.js

--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -4,8 +4,15 @@ const bcrypt = require("bcryptjs")
 const UserSchema = new Schema(
 	{
 		googleId: String,
-		password: String,
-		email: String,
+		password: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
 		userName: String,
 		name: String,
 		surname: String,
